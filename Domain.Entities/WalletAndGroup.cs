98dc4dfa3aaab2833a664cd7e46fdbc66666524e
Денис Бикбов <denis-bikbov@yandex.ru>
2024-10-29 21:52:10 +0400

    добавлена сущность WalletAndGroup

---
+++ Domain.Entities/WalletAndGroup.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FamilyWallet.Domain
+{
+    [Table("Users")]
+    internal class WalletAndGroup
+    {
+        [Column]
+        public string WalletId { get; set; }
+
+        [Column]
+        public string GroupId { get; set; }
+
+        [Column]
+        public string PrivilagesId { get; set; }
+    }
+}
